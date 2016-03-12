009efc5f587d9829c7cdb101c0f168f8875b8d86
Alexander Gitovich <a.gitovich@gmail.com>
2016-03-12 14:46:49 +0300

    Added primitive flask app with webhook and settings

--- src/app.py
+++ src/app.py
@@ -2,3 +2,28 @@
 """
 created by artemkorkhov at 2016/03/12
 """
+
+
+import logging
+
+import requests
+
+from flask import Flask, request
+
+
+app = Flask(__name__)
+app.config.from_object('settings')
+
+app.logger.setLevel(logging.INFO)
+r = requests.get(app.config["TELEGRAM"]["api_uri"].format(app.config["TELEGRAM"][
+                 "token"], "setWebhook"), {"url": app.config["TELEGRAM"]["callback_uri"]})
+print r.status_code
+
+
+def handle():
+    app.logger.info(request.json)
+    print request.json
+    return "Ok"
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/settings.py
@@ -0,0 +1,10 @@
+TELEGRAM = {
+    "token": "185093347:AAHbhPcP3xPj7kiL3vpBUxM1lcxqmQR9WH8",
+    "api_uri": "https://api.telegram.org/bot{}/{}",
+    "callback_uri": "mrxmoscow.com:5000/aaa"
+}
+
+try:
+    from settings_local import *
+except ImportError:
+    pass
