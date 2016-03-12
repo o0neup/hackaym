55b6a99307e5045d943e727406bf2093c86f53ba
Alexander Gitovich <a.gitovich@gmail.com>
2016-03-13 01:20:04 +0300

    question sequence

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 import logging
 
+import re
 import telebot
 from telebot import types
 
@@ -12,9 +13,36 @@ telebot.logger.setLevel(logging.INFO)
 logger = telebot.logger
 
 
+def parse_username(text):
+    logger.info("Parse username in '{}'".format(text))
+    if re.search("@\w+", text):
+        return re.search("@\w+", text).group(0)
+    return None
+
+
+def parse_amount(text):
+    logger.info("Parse amount in '{}'".format(text))
+    try:
+        return float(text)
+    except ValueError:
+        return None
+
 command_dict = {
-    "bill": ["for whom?", "amount", "describe it"]
+    "bill": [
+        {
+            "text": "for whom?",
+            "parser": parse_username,
+            "fallback_message": "Must be a username"
+        }, {
+            "text": "amount",
+            "parser": parse_amount,
+            "fallback_message": "Must be a float value"
+        }, {
+            "text": "describe it"
+        }
+    ]
 }
+# fallback message
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -28,6 +56,8 @@ def send_welcome(message):
 def handle_bill(message):
     logger.info(message)
     write_to_storage(message.from_user.id, message.chat.id, message.text)
+    bot.send_message(message.chat.id, command_dict[
+                     message.text.strip('/')][0]["text"], reply_markup=types.ForceReply())
 
 
 @bot.message_handler(func=lambda message: True)
@@ -35,24 +65,52 @@ def save_user(message):
     logger.info(message)
     logger.info("User id: '{}', username: '{}'".format(
         message.from_user.id, message.from_user.username))
+    key = storage_key(message.from_user.id, message.chat.id)
+    if key in storage:
+        messages = storage[key]
+        command = messages[0].strip('/')
 
-    if get_key(message.from_user.id, message.chat.id) in storage:
-        write_to_storage(message.from_user.id, message.chat.id, message.text)
+        question = command_dict[command][len(messages) - 1]
+        next_question = None
+        if len(messages) < len(command_dict[command]):
+            next_question = command_dict[command][len(messages)]
 
+        print messages, command, question
+        logger.info("Message in storage with \nkey '{}',\nmessages: {}\nquestion: {}".format(
+            key, messages, question))
+
+        def save_and_send_next(text):
+            write_to_storage(message.from_user.id,
+                             message.chat.id, text)
+            if next_question is not None:
+                bot.send_message(message.chat.id, next_question["text"])
+            else:
+                logger.info("Question sequence is over")
+
+        if "parser" in question:
+            if question["parser"](message.text) is not None:
+                parsed_message = question["parser"](message.text)
+                logger.info("Parsed message: '{}'".format(parsed_message))
+                save_and_send_next(parsed_message)
+            else:
+                bot.send_message(message.chat.id, question["fallback_message"])
+                bot.send_message(message.chat.id, question[
+                                 "text"], reply_markup=types.ForceReply())
+        else:
+            save_and_send_next(message.text)
 storage = {}
 
 
-def get_key(user_id, chat_id):
-    return "{}{}".format(user_id, chat_id)
+def storage_key(user_id, chat_id):
+    return "{},{}".format(user_id, chat_id)
 
 
-def write_to_storage(user_id, chat_id, text):
-    key = get_key(user_id, chat_id)
-    if key not in storage or text.startswith('/'):
+def write_to_storage(user_id, chat_id, value):
+    key = storage_key(user_id, chat_id)
+    if key not in storage or (isinstance(value, str) and value.startswith('/')):
         storage[key] = []
 
-    storage[key].append(text)
-    print storage
+    storage[key].append(value)
 
 
 bot.polling()
