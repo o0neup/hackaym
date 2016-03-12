12ae84111d592b3c302d503239818c30b171c707
Alexander Gitovich <a.gitovich@gmail.com>
2016-03-12 16:42:25 +0300

    fixed callback_uri

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ BOT_TOKEN = "**SECRET**"
 TELEGRAM = {
     "token": "185093347:AAHbhPcP3xPj7kiL3vpBUxM1lcxqmQR9WH8",
     "api_uri": "https://api.telegram.org/bot{}/{}",
-    "callback_uri": "https://mrxmoscow.com:5000/aaa"
+    "callback_uri": "https://mrxmoscow.com/aaa"
 }
 
 
