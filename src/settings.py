1f19b6fbbbc9867b2e68516ea68d1ccd437cbb86
Alexander Gitovich <a.gitovich@gmail.com>
2016-03-12 16:28:06 +0300

    testing webhooks

--- src/settings.py
+++ src/settings.py
@@ -1,7 +1,7 @@
 TELEGRAM = {
     "token": "185093347:AAHbhPcP3xPj7kiL3vpBUxM1lcxqmQR9WH8",
     "api_uri": "https://api.telegram.org/bot{}/{}",
-    "callback_uri": "mrxmoscow.com:5000/aaa"
+    "callback_uri": "https://mrxmoscow.com:5000/aaa"
 }
 
 try:
